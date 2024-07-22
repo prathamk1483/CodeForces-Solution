5c69db8b7e212086e6acaed8531622a6f142dd12
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-07-22 15:28:37 +0530

    Update B. Array Craft.java

--- Codeforces Round 960 (Div. 2)/B. Array Craft.java
+++ Codeforces Round 960 (Div. 2)/B. Array Craft.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
-public class Solution{
-
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while(t-- >0){
-            int n = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int arr[] = new int[n];
-
-            for(int i=y-1;i>=0;i-=2){
-                arr[i] = -1;
-            }
-            for(int i=y-1;i>=0;i-=2) arr[i] = 1;
-
-            for(int i=y;i<x;i++) arr[i] = 1;
-
-            for(int i=x;i<arr.length;i+=2) arr[i] = -1;
-            for(int i=x+1;i<arr.length;i+=2) arr[i] = 1; 
-
-            for(int i=0;i<arr.length;i++) System.out.print(arr[i]+" ");
-            System.out.println();
-            
-        }
-    }
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      while(t-- >0){
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        x--;y--;
+        int arr[] = new int[n];
+    
+        for(int i=y;i<=x;i++) arr[i] = 1;
+        
+        for(int i=y-1;i>=0;i--) arr[i] = ((y-i)%2 == 1) ? -1 :1; 
+        for(int i=x+1;i<arr.length;i++) arr[i] = ((i-x)%2 == 1 ) ? -1 :1; 
+        
+        for(int i: arr) System.out.print(i+" ");
+        System.out.println();
+      }
+  }
 }
